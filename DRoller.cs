5a470d49ac91dcb9c5e8d47cea410fea3e2b0252
benroper <benroper@BENROPER9609>
2024-01-17 17:49:55 -0700

    Add project files.

---
+++ DRoller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class DRoller
+    {
+        // below calls the method to simulate the dice rolls
+        public int[] SimulateRolls(int nRolls)
+        {
+            int[] results = new int[13];
+
+            Random random = new Random();
+            // for loop to run through however many times I want to roll the die
+            for (int i = 0; i < nRolls; i++)
+            {
+                // simulate the roll of two die
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+                int sum = dice1 + dice2;
+
+                // increment the corresponding result array element
+                results[sum]++;
+            }
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using Mission2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        // get user input for the number of rolls
+        Console.Write("How many dice rolls would you like to simulate:");
+
+        int nRolls = int.Parse(Console.ReadLine());
+        // create an instance of the DRoller class and simulate dice rolls
+        DRoller dRoller = new DRoller();
+
+        int[] results = dRoller.SimulateRolls(nRolls);
+
+        // print the histogram
+        PrintHistogram(results, nRolls);
+
+        Console.ReadLine(); 
+
+    }
+    // class to print the histomgram of *'s
+    static void PrintHistogram(int[] results, int nRolls)
+    {
+        Console.WriteLine("\nHistogram:");
+
+        for (int i = 2; i <= 12; i++)
+        {
+            // calculate the percentage for each number and print the corresponding num of *'s
+            int percentage = results[i] * 100 / nRolls;
+            Console.WriteLine($"{i}: {new string('*', percentage)}");
+        }
+    }
+}
